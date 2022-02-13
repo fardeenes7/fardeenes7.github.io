83f6f4b9883bdf9294e6f56bb39d74d01e3e067d
fardeenes7 <fardeen.es7@gmail.com>
2022-02-14 00:48:45 +0600

    package.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useRef, useEffect} from 'react';
+import React, {useEffect} from 'react';
 import Navbar from '../components/Navbar';
 import loader from '../assets/loader.svg';
 import Aos from 'aos';
