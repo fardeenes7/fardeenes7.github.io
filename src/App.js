105de4e38a0b4427b51d00a88f774a2ef396bc95
Fardeen Ehsan <fardeen.es7@gmail.com>
2023-01-11 19:13:05 +0600

    maintenance mode

--- src/App.js
+++ src/App.js
@@ -1,34 +1,30 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Maintenance from './maintenance/Maintenance';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Blog from './pages/Blog';
-import SingleBlog from './pages/SingleBlog';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Maintenance from "./maintenance/Maintenance";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Portfolio from "./pages/Portfolio";
+import Blog from "./pages/Blog";
+import SingleBlog from "./pages/SingleBlog";
 
 function App() {
-  const isMaintenance = false;
-  if (isMaintenance) {
-    // dev code
-    return (
-      <Maintenance/>
-      
-    );
-} else {
-    // production code
-    return (
-      <Router>
-      <Routes>
-        <Route path='/' element={<Maintenance/>} />
-        <Route path='/contact' element={<Contact/>} />
-        <Route path='/portfolio' element={<Portfolio/>} />
-        <Route path='/blog/' element={<Blog/>} />
-        <Route path='/blog/:slug' element={<SingleBlog/>} />
-      </Routes>
-    </Router>
-    );
-}
-  
+    const isMaintenance = true;
+    if (isMaintenance) {
+        // dev code
+        return <Maintenance />;
+    } else {
+        // production code
+        return (
+            <Router>
+                <Routes>
+                    <Route path="/" element={<Maintenance />} />
+                    <Route path="/contact" element={<Contact />} />
+                    <Route path="/portfolio" element={<Portfolio />} />
+                    <Route path="/blog/" element={<Blog />} />
+                    <Route path="/blog/:slug" element={<SingleBlog />} />
+                </Routes>
+            </Router>
+        );
+    }
 }
 
 export default App;
